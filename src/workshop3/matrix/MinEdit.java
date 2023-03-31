fef19d0ba2df3dcabc9b2799c356eb4905cf1f1f
RAJ JAISWAL <rjjaiswal467@gmail.com>
2023-03-31 23:22:32 +0530

    min Edit

---
+++ src/workshop3/matrix/MinEdit.java
@@ -0,0 +1,33 @@
+package workshop3.matrix;
+
+public class MinEdit {
+    public static int min_Distance(String s1, String s2) {
+        int m = s1.length();
+        int n = s2.length();
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = i;
+        }
+
+        for (int j = 0; j <= n; j++) {
+            dp[0][j] = j;
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = 1 + Math.min(dp[i - 1][j - 1], Math.min(dp[i][j - 1], dp[i - 1][j]));
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+
+    public static void main(String[] args) {
+        min_Distance("Raj","Rama");
+    }
+}
